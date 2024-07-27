f8ce092e7f0e805fb66cb9562f9d341aad5bedd5
benson he <hebinboss@gmail.com>
2024-07-27 13:15:18 +0800

    Truth initial
    Add all component

--- set_ssserver.sh
+++ set_ssserver.sh
@@ -12,8 +12,12 @@ fi
 systemctl stop firewalld
 echo "######"
 # 2
+yum install python3 python3-pip
 pip3 install shadowsocks
 echo "######"
+my_python_version=`python -V | awk '{print $2}'`
+my_python_version_2digit=${my_python_version:0:3}
+sed -i 's/EVP_CIPHER_CTX_cleanup/EVP_CIPHER_CTX_reset/g' /usr/local/lib/python${my_python_version_2digit}/site-packages/shadowsocks/crypto/openssl.py
 # 3
 server_ip=`ip addr | grep inet | egrep -v 'inet6|127' | awk '{print $2}' | awk -F "/" '{print $1}'`
 
@@ -32,3 +36,23 @@ EOF
 echo "######"
 # 4
 ssserver -c /etc/shadowsocks.json -d start
+# 5
+testing=$(head -2 /var/log/shadowsocks.log)
+#echo -e $testing
+
+generate_post_data()
+{
+cat <<EOF
+{
+        "channel": "#monitoring-infrastructure",
+        "icon_emoji": ":ghost:",
+        "username": "ss-notice",
+        "blocks":[{"type":"section","text":{"type":"mrkdwn","text":"*_fancy meassages:_*$testing"}}]
+}
+EOF
+}
+
+curl -i -H "Accept: application/json" -H "Content-type: application/json" \
+--data "$(generate_post_data)" -X POST https://hooks.slack.com/services/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
+
+dd if=/dev/zero of=/root/loopdev bs=1M count=100
