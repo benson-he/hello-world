4153880dd63ec2eff41299b2413bff1677ead9f5
benson he <hebinboss@gmail.com>
2025-03-06 18:06:37 +0800

    Update set_ssserver.sh
    Change password to variable

--- set_ssserver.sh
+++ set_ssserver.sh
@@ -27,7 +27,7 @@ cat << EOF > /etc/shadowsocks.json
     "server_port": 9094,
     "local_address": "127.0.0.1",
     "local_port": 1080,
-    "password": "MySsPass4!",
+    "password": "$SS_PASSWORD",
     "timeout": 300,
     "method": "aes-256-cfb",
     "fast_open": false
